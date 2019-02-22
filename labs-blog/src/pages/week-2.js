87a9b3b976ce89c325fef850cbf6a468c663fd8a
chainchompa <chainchompa@gmail.com>
2019-02-22 09:41:23 -0500

    added week 2 pictures and detailed analysis

--- labs-blog/src/pages/week-2.js
+++ labs-blog/src/pages/week-2.js
@@ -9,13 +9,20 @@ const WeekTwo = () => (
     <h2>2/18/2019-2/22/2019</h2>
     <img src={require('../images/contributionchartweek2.png')} />
     <p>Github: chainchompa</p>
-    <p>This week I finally finished the Billing page, and got the back end
-    of the project working with Stripe. Now we can successfully take payments both locally and while deployed on Netlify. 
-    I also worked more on the User Settings Page, and am currently still working to integrate it with Firebase and our database. I 
-    also added a way to add a profile picture for the representative in User Settings, and still need to store the uploaded picture into our 
-    Cloudlify server. Even though my contributions were less this week, I still feel like I accomplished a lot because I also was pair programming
-    and helping other people with issues throughout the week. Such as helping Wonjae deploy the chat app to Netlify, helping Linda with the Admin Panel,
-    and working with the team to help make sure we are branching correctly and fixing merge conflicts.</p>
+    <p>
+      This week I finally finished the Billing page, and got the back end of the
+      project working with Stripe. Now we can successfully take payments both
+      locally and while deployed on Netlify. I also worked more on the User
+      Settings Page, and am currently still working to integrate it with
+      Firebase and our database. I also added a way to add a profile picture for
+      the representative in User Settings, and still need to store the uploaded
+      picture into our Cloudlify server. Even though my contributions were less
+      this week, I still feel like I accomplished a lot because I also was pair
+      programming and helping other people with issues throughout the week. Such
+      as helping Wonjae deploy the chat app to Netlify, helping Linda with the
+      Admin Panel, and working with the team to help make sure we are branching
+      correctly and fixing merge conflicts.
+    </p>
     <div className="links">
       <p>
         Working on writing the backend code for Stripe
@@ -44,9 +51,7 @@ const WeekTwo = () => (
         <a href="https://github.com/Lambda-School-Labs/labs10-webchat/pull/75">
           Pull Request
         </a>
-        <a href="https://trello.com/c/0xN98Qsk/21-admin-panel">
-          Trello Card
-        </a>
+        <a href="https://trello.com/c/0xN98Qsk/21-admin-panel">Trello Card</a>
       </p>
     </div>
     <div className="links">
@@ -55,14 +60,13 @@ const WeekTwo = () => (
         <a href="https://github.com/Lambda-School-Labs/labs10-webchat/pull/66">
           Pull Request
         </a>
-        <a href="https://trello.com/c/1iHIAS4W/15-landing-page">
-          Trello Card
-        </a>
+        <a href="https://trello.com/c/1iHIAS4W/15-landing-page">Trello Card</a>
       </p>
     </div>
     <div className="links">
       <p>
-        Changing Billing environment variable based off of whether in production or development
+        Changing Billing environment variable based off of whether in production
+        or development
         <a href="https://github.com/Lambda-School-Labs/labs10-webchat/pull/77">
           Pull Request
         </a>
@@ -71,6 +75,23 @@ const WeekTwo = () => (
         </a>
       </p>
     </div>
+    <h3>Detailed Analysis</h3>
+    <p>
+      Most of my time this week was spent on the Billing component and writing
+      the backend code for Stripe. I had to write a function to find a company
+      by ID and a function to change the has_paid boolean to true for that
+      company, so that once we have the Company accounts set up on the front end
+      we will be marking the correct company as paid. I also decided to hold off
+      on charging monthly for the service and leave that as a stretch goal, so
+      companies are now only having to pay a one time fee to use the WebChat
+      service. We now have it so that whether you are testing from localhost or
+      from the deployed Netlify site, it will still use the correct url to post
+      to. So we can use the Billing page from either.
+    </p>
+    <img src={require('../images/week2pic1.png')} />
+    <img src={require('../images/week2pic2.png')} />
+    <img src={require('../images/week2pic3.png')} />
+
     <Link to="/">
       <h4>Go back to the homepage</h4>
     </Link>
